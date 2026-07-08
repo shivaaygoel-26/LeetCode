9ae5e3b44cfd69255529050c220ed6dfc10f5762
shivaaygoel-26 <shivaaygoel78@gmail.com>
2026-07-08 21:45:26 +0530

    Time: 0 ms (100%), Space: 9.9 MB (55.28%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        int i=0,j=s.length()-1;
+        while(j>i){
+            if(!isalnum(s[i])){
+                i++;
+            }else if(!isalnum(s[j])){
+                j--;
+            }else if(s[i]!=s[j]){
+                return false;
+            }
+               else {
+        i++;
+        j--;
+               }
+        }
+        return true;
+    }
+
+};
