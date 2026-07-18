41d0938b717428645afcb03b390952719f0c60cc
shivaaygoel-26 <shivaaygoel78@gmail.com>
2026-07-18 14:08:22 +0530

    Time: 2158 ms (5.01%), Space: 8.3 MB (5.26%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+      unordered_set<int>factors;
+       for (int i=1;i<=num;i++){
+        if(num%i==0){
+            factors.insert(i);
+        }
+       }
+        int sum = 0;
+        factors.erase(num);
+for(int x : factors) {
+    sum += x;
+}
+if(sum==num){
+    return true;
+}
+    return false;
+    }
+};
