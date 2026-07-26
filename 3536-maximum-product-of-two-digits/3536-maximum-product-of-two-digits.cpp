c9cd95931b6107b400714c7b518c02f0ffcd5171
shivaaygoel-26 <shivaaygoel78@gmail.com>
2026-07-26 22:14:39 +0530

    Time: 0 ms (100%), Space: 8.7 MB (73.98%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -5,12 +5,12 @@ public:
     while (n>0){
         digit=n%10;
         n=n/10;
-        if(digit>largest2){
-        largest2=digit;}
         if(digit>largest){
             largest2=largest;
             largest=digit;
         }
+        else if(digit>largest2){
+        largest2=digit;}
     }
     return largest2*largest;
     }
