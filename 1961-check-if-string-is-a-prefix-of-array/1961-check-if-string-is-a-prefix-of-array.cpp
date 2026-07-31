68cd4af278df0581e4cd27cfbdd6a32684851375
shivaaygoel-26 <shivaaygoel78@gmail.com>
2026-07-31 21:09:37 +0530

    Time: 0 ms (100%), Space: 16.8 MB (75.21%) - LeetHub

---
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPrefixString(string s, vector<string>& words) {
+        string ans = "";
+
+        for (int i = 0; i < words.size(); i++) {
+            ans += words[i];
+            if (ans == s) {
+                return true;
+            }
+            if (ans.size() > s.size()) {
+                return false;
+            }
+        }
+        return false;
+    }
+};
