6b7510e590e52108bfd70683a90e624f01af22a1
shivaaygoel-26 <shivaaygoel78@gmail.com>
2026-08-06 21:25:05 +0530

    Time: 0 ms (100%), Space: 9.5 MB (21.22%) - LeetHub

---
+++ 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> evenOddBit(int n) {
+        int even = 0;
+        int odd = 0;
+        int index = 0;
+        while(n > 0) {
+            if(n & 1) {
+                if(index % 2 == 0)
+                    even++;
+                else
+                    odd++;
+            }
+            n = n >> 1;
+            index++;
+        }
+        return {even, odd};
+    }
+};
