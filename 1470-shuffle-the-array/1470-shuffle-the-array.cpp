be43cd2e5566e53a29ab78adab2e42605db85ab2
shivaaygoel-26 <shivaaygoel78@gmail.com>
2026-08-13 21:12:28 +0530

    Time: 0 ms (100%), Space: 13.5 MB (53.59%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        int i=0,j=n;
+        vector<int>ans;
+        while(i!=n){
+            ans.push_back(nums[i]);
+            ans.push_back(nums[j]);
+            i++;
+            j++;
+        }
+        return ans;
+    }
+};
