5cc409d3c13b95263290cb5aef00534bba347276
shivaaygoel-26 <shivaaygoel78@gmail.com>
2026-08-22 16:58:07 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (14.31%) - LeetSync

---
+++ 3371-harshad-number/harshad-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+      int n=x;
+      int sum=0;
+      while(n>0){
+        int temp=n%10;
+        sum+=temp;
+        n=n/10;
+      }  
+      if(x%sum==0){
+        return sum;
+      }
+      return -1;
+    }
+};
